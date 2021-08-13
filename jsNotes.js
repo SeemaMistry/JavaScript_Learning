1fa36c3c113ba8e6406d4eb87e134616a435f01b
SeemaMistry <seema.m.1996@gmail.com>
2021-08-13 17:40:51 -0400

    Learn 3 different methods of escaping quotes in JS strings

--- jsNotes.js
+++ jsNotes.js
@@ -1,5 +1,8 @@
 /* Followings Beginners Tutorial: https://www.youtube.com/watch?v=PkZNo7MFNFg
 Current Stop Time Stamp: 24:58
+How to run code in terminal: 
+1) Go into correct folder -> cd JavaScript_Learning
+2) Run code using node.j -> node myfile.js
 */
 
 
@@ -30,3 +33,23 @@ console.log(a)
 var num = 10;
 num++;
 console.log(num) 
+
+/* Strings: Declare, escape literal quotes, quoting strings
+There are different ways to by-pass the literal quotes inside a string
+
+what you are tryig to print to the screen:
+I am a "double quoted" string inside "double quotes."
+*/ 
+// Method 1: the backslash prior to quote
+var myStr = "I am a \"double quoted\" string inside \"double quotes.\""
+console.log(myStr)
+
+// Method 1: single quotes to encase full string
+var myStr = 'I am a "double quoted" string inside "double quotes."'
+console.log(myStr)
+
+// Method 1: back ticks (on ~ button) to encse full string (allows use of both single and double quotes to be printed)
+var myStr = `'I am a "double quoted" string inside "double quotes."'`
+console.log(myStr)
+
+
