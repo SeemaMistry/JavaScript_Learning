649c914c799a3d135ee36cf6bdb12656b8ec67c4
SeemaMistry <seema.m.1996@gmail.com>
2021-08-13 18:19:50 -0400

    Create new file madlibs.js. Make a madlibs style game.

---
+++ madlibs.js
@@ -0,0 +1,7 @@
+/* Followings Beginners Tutorial: https://www.youtube.com/watch?v=PkZNo7MFNFg
+Start Time: 36:29
+Current Stop Time Stamp: 
+How to run code in terminal: 
+1) Go into correct folder -> cd JavaScript_Learning
+2) Run code using node.j -> node myfile.js
+*/
