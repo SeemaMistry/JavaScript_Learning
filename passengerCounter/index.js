f1c4b584208fe8081068e05ad5fb0ca9fdf2d1b0
SeemaMistry <seema.m.1996@gmail.com>
2021-08-25 18:24:22 -0400

    Reset before stating button functions

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -1,23 +1,2 @@
 // document.getElementById("count-el").innerText = 5;
 
-// store your count in a variable
-let count = 50;
-console.log(count);
-count += 50;
-console.log(count);
-count -= 75;
-console.log(count);
-count += 45;
-console.log(count);
-
-
-
-// create var myAge, enter your age, then log it to console
-// var myAge = 25;
-// console.log(myAge)
-
-// log your age in dog years
-// let myAge = 25;
-// let humanDogRatio = 7;
-// let myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge);
