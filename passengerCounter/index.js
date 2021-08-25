dbeffd27e928bc46c3e61a7e1a20c1a5c027f3c5
SeemaMistry <seema.m.1996@gmail.com>
2021-08-25 16:44:19 -0400

    Play around with JS using variables storing bonus points.

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -1,14 +1,23 @@
 // document.getElementById("count-el").innerText = 5;
 
 // store your count in a variable
-let count = 0;
+let count = 50;
+console.log(count);
+count += 50;
+console.log(count);
+count -= 75;
+console.log(count);
+count += 45;
+console.log(count);
+
+
 
 // create var myAge, enter your age, then log it to console
 // var myAge = 25;
 // console.log(myAge)
 
 // log your age in dog years
-let myAge = 25;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
-console.log(myDogAge);
+// let myAge = 25;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
