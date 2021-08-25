fe5dcd7ea2f511b88988cc3ab93f611b87d3c0f6
SeemaMistry <seema.m.1996@gmail.com>
2021-08-25 18:50:21 -0400

    Create function increment() that increments count onclick and reflects count on screen.

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -1,2 +1,10 @@
 // document.getElementById("count-el").innerText = 5;
 
+// initialize count to 0
+let count = 0;
+// listen for clicks and increment count
+function increment() {
+    count += 1;
+    // change count-et in html to reflect new count
+    document.getElementById("count-el").innerText = count;
+}
