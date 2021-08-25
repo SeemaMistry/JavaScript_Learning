ffc9f1e91ddb2eb1a258470e48e7848be442e164
SeemaMistry <seema.m.1996@gmail.com>
2021-08-25 16:30:39 -0400

    Play around with JS using variables storing ages.

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -1,1 +1,14 @@
-document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
+
+// store your count in a variable
+let count = 0;
+
+// create var myAge, enter your age, then log it to console
+// var myAge = 25;
+// console.log(myAge)
+
+// log your age in dog years
+let myAge = 25;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
