c15691f4aa70d319c02594ce2752e5e0823a4ced
SeemaMistry <seema.m.1996@gmail.com>
2021-09-03 13:15:02 -0400

    Add inventory count (set at 5) which decrements each time the item is purchased. Once inventory is 0, give error message to user.

--- ErrorMessage/index.js
+++ ErrorMessage/index.js
@@ -1,6 +1,16 @@
-let errorMSG = document.getElementById("error-msg")
+// let errorMSG = document.getElementById("error-msg")
+let purchaseMSG = document.getElementById("purchased-msg")
+
+let inventory = 5;
 
 function purchase() {
-    console.log("Button clicked")
-    errorMSG.textContent = "Something went wrong, please try again"    
+    if (inventory > 0) {
+        inventory -= 1
+        purchaseMSG.textContent = "Thank you for your purchase"
+    }
+    else {
+        purchaseMSG.textContent = "Something went wrong, please try again"    
+        // errorMSG.textContent = "Something went wrong, please try again"    
+
+    }
 }
