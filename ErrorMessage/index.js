466d6755ca68dd97c8d597a9fe11631e081d84e6
SeemaMistry <seema.m.1996@gmail.com>
2021-09-03 12:58:07 -0400

    Initalize purchase() that prints error message to <p id=error-msg>

---
+++ ErrorMessage/index.js
@@ -0,0 +1,6 @@
+let errorMSG = document.getElementById("error-msg")
+
+function purchase() {
+    console.log("Button clicked")
+    errorMSG.textContent = "Something went wrong, please try again"    
+}
