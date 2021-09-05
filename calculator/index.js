ae36b8cc8d10bb1975fd7cbba06de216ad84262a
SeemaMistry <seema.m.1996@gmail.com>
2021-09-04 21:28:20 -0400

    Add javascript functions for all four buttons on click to apply appropriate mathimatical operation and print to sum.

--- calculator/index.js
+++ calculator/index.js
@@ -3,3 +3,20 @@ let num2 = 2
 
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+let sum = document.getElementById("sum")
+function add() {
+    sum.innerHTML = sum.innerHTML + (num1+num2)
+}
+
+function subtract() {
+    sum.innerHTML = sum.innerHTML + (num1-num2)
+}
+
+function mutiply() {
+    sum.innerHTML = sum.innerHTML + (num1*num2)
+}
+
+function divide() {
+    sum.innerHTML = sum.innerHTML + (num1/num2)
+}
