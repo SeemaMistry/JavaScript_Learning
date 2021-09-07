324ed0ac508f3baf83b3043bbf311c67baa1a208
SeemaMistry <seema.m.1996@gmail.com>
2021-09-07 17:05:22 -0400

    Initalize index.html/css/js for the blackjack game.

--- calculator/index.js
+++ calculator/index.js
@@ -4,6 +4,7 @@ let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
+
 let sum = document.getElementById("sum")
 function add() {
     sum.innerHTML = "Sum: " + (num1+num2)
