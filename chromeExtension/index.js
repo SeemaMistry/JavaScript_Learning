02ecc38a94df3b27b6f17bfb25ac7e3393133103
SeemaMistry <seema.m.1996@gmail.com>
2021-09-22 15:21:29 -0400

    Refactor index.js to use .addEventListener() instead of calling onclick=function() in html.

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,5 +1,23 @@
 let saveEL = document.getElementById("save-el")
+/* This is one method of creating an event listener:
+    function saveLead() {
+        saveEL.textContent = "Button clicked!"
+    }
+Requires:
+    - onclick="function()" in html attribute
+    - function() made in js document
 
-function saveLead() {
+However, there is a faster and simiplier way of creating an event listener:
+    [element].addEventListener(eventType, function(){
+        executable code on event
+    })
+Requires:
+    - NO html onclick="function()" needed
+    - onclick and function written inside addEventListener parameters
+    - NOTE: no function naming required
+*/ 
+
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function () {
     saveEL.textContent = "Button clicked!"
-}
+})
