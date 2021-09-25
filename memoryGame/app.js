973530dc658982a255a3bc28b75aa978c8e1ca36
SeemaMistry <seema.m.1996@gmail.com>
2021-09-24 23:54:24 -0400

    Create cardArray which stores each card as an object. There will be 2 of each card for the matching game.

---
+++ memoryGame/app.js
@@ -0,0 +1,57 @@
+// card options. Make 2 of each card
+const cardArray = [
+// burger
+    {
+        name: "burger",
+        img: "images/burger.png"
+    },
+    {
+        name: "burger",
+        img: "images/burger.png"
+    },
+// chicken bucket
+    {
+        name: "chickenBucket",
+        img: "images/chickenBucket.png"
+    },
+    {
+        name: "chickenBucket",
+        img: "images/chickenBucket.png"
+    },
+// chocolate bar
+    {
+        name: "chocolateBar",
+        img: "images/chocolateBar.png"
+    },
+    {
+        name: "chocolateBar",
+        img: "images/chocolateBar.png"
+    },
+// fries
+    {
+        name: "fries",
+        img: "images/fries.png"
+    },
+    {
+        name: "fries",
+        img: "images/fries.png"
+    },
+// hotdog
+    {
+        name: "hotdog",
+        img: "images/hotdog.png"
+    },  
+    {
+        name: "hotdog",
+        img: "images/hotdog.png"
+    },      
+// pizzaSlice
+    {
+        name: "pizzaSlice",
+        img: "images/pizzaSlice.png"
+    },
+    {
+        name: "pizzaSlice",
+        img: "images/pizzaSlice.png"
+    }
+]
