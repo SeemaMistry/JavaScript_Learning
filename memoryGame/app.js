fd7263115a3df7dea2759beaa376bada256fd5c9
SeemaMistry <seema.m.1996@gmail.com>
2021-09-25 00:46:15 -0400

    Create game board by creating img elements for each card with the pattern background. Render cards to the screen.

--- memoryGame/app.js
+++ memoryGame/app.js
@@ -55,3 +55,27 @@ const cardArray = [
         img: "images/pizzaSlice.png"
     }
 ]
+
+// create the game board
+const grid = document.querySelector(".grid")
+
+// loop over cardArray and create image elements of each card
+function createBoard() {
+    for (let i=0; i < cardArray.length; i++) {
+        // for each card create an img element with the pattern as the back of the card
+        // give each card id from 0-11
+        // add imag element to .grid class in html file
+        var card = document.createElement('img') 
+        card.setAttribute("src", "images/pattern.png") 
+        card.setAttribute("data-id", i) 
+        // card.addEventListener("click", flipCard)
+        grid.appendChild(card) // all the cards are img elements with ids, appendchild adds them to the .grid class in the html file
+
+    }
+}
+
+createBoard()
+
+
+
+
