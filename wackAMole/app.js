6e7ae51456961e85c4d122690cf01f0b52fd03a3
SeemaMistry <seema.m.1996@gmail.com>
2021-09-26 20:35:45 -0400

    Using innerHtml create 9 div containers inside the grid class with class=square and ids from 1 to 9

---
+++ wackAMole/app.js
@@ -0,0 +1,15 @@
+// get the grid class div and add in 9 html divs, ids 1-9 and class=square
+const grid = document.querySelector(".grid")
+
+// store all the div elements as a string
+let divContainers = ""
+// loop and make 9 div, class=square, id=i
+for (let i = 1; i < 10; i++) {
+    // html div class
+    divContainers += `
+    <div class=square id=${i}>
+    </div>
+    `
+}
+grid.innerHTML = divContainers
+// call the DOM .innerHtml
