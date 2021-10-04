5237213e476b44dc5e7c07cab0aaa44e04114cb9
SeemaMistry <seema.m.1996@gmail.com>
2021-10-03 21:04:45 -0400

    Retrieve DOM in js file.

---
+++ tickTacToe/app.js
@@ -0,0 +1,12 @@
+// DOM
+const grid = document.querySelector(".grid")
+const allCells = document.querySelectorAll(".cell")
+
+const exitBtn = document.querySelector("#exit-btn")
+const anotherRoundBtn = document.querySelectorAll("#anotherRound-btn")
+
+const xScoreEl = document.querySelector("#xscore-el")
+const oScoreEl = document.querySelector("#oscore-el")
+
+let xPoints = 0
+let oPoints = 0
