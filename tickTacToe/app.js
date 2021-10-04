e4165a39ddd2127f1fd86d54d90c4764fcb48194
SeemaMistry <seema.m.1996@gmail.com>
2021-10-03 21:49:23 -0400

    Add function that when mouse-over occurs, shows players mark on the square.

--- tickTacToe/app.js
+++ tickTacToe/app.js
@@ -1,34 +1,35 @@
-// DOM
+//////////////// DOM ////////////////
 const grid = document.querySelector(".grid")
 const allCells = document.querySelectorAll(".cell")
-
+ 
 const exitBtn = document.querySelector("#exit-btn")
 const anotherRoundBtn = document.querySelectorAll("#anotherRound-btn")
 
 const xScoreEl = document.querySelector("#xscore-el")
 const oScoreEl = document.querySelector("#oscore-el")
 
-// SORT ALL THE ROWS
+//////////////// SORT ALL THE ROWS ////////////////
 const row0 = [allCells[0], allCells[1], allCells[2]]
 const row1 = [allCells[3], allCells[4], allCells[5]]
 const row2 = [allCells[6], allCells[7], allCells[8]]
 const rows = [row0, row1, row2]
 
-// SORT ALL THE COLUMNS
+//////////////// SORT ALL THE COLUMNS ////////////////
 const col0 = [allCells[0], allCells[3], allCells[6]]
 const col1 = [allCells[1], allCells[4], allCells[7]]
 const col2 = [allCells[2], allCells[5], allCells[8]]
 const columns = [col0, col1, col2]
 
 
-// VARIABLES 
+//////////////// VARIABLES ////////////////
 let gameIsLive = true
 let xTurn = true
 
 let xPoints = 0
 let oPoints = 0
 
-// UTILITY FUNCTIONS 
+//////////////// UTILITY FUNCTIONS ////////////////
+
 /* getCellClassList (cell) -> [...classList]
     Require: cell = cell Object
     Purpose: get an array containing the cell's classList
@@ -51,8 +52,15 @@ const getCellLocation = (cell) => {
     return [parseInt(rowIndex, 10), parseInt(colIndex, 10)]
     
 }
+/* addPlayerMarkToCellClassList(cell) -> null
+    Require: cell = cell object
+    Purpose: Add "X" or "O" to cell's classList based on whos turn it is
+*/
+const addPlayerMarkToCellClassList = (cell) => {
+    cell.classList.add(xTurn ? "X" : "O")
+}
 
-// HANDLE EVENTS
+//////////////// HANDLE EVENTS ////////////////
 
 /* handleMouseOver(e)
     Require: e = event on a cell
@@ -61,11 +69,13 @@ const getCellLocation = (cell) => {
 const handleMouseOver = (e) => {
     // get cell location 
     const cell = e.target
+    addPlayerMarkToCellClassList(cell)
     const [rowIndex, colIndex] = getCellLocation(cell)
+
     
 }
 
-// ADD EVENTS
+//////////////// ADD EVENTS ////////////////
 for (const row of rows) {
     for (const cell of row) {
         // hover on
