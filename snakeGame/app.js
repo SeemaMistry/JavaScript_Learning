f989fa36b28b3a61a6565f8138ad7e3f64db49c2
SeemaMistry <seema.m.1996@gmail.com>
2021-10-05 01:13:58 -0400

    Remove corner wall overlap making a plus sign. In css file, get location of corner and change size of div to 10px by 10px.

--- snakeGame/app.js
+++ snakeGame/app.js
@@ -20,28 +20,28 @@ for (let i=0; i < 12; i++) {
         cellCount++
 
         // add walls 
-        squares += (i ===1 && !(k ===2 || k ===3 || k ===8 || k ===9)) ? `wall-top ` : ``
-        squares += (i ===10 && !(k ===2 || k ===3 ||  k ===8 || k ===9)) ? `wall-bottom ` : ``
-        squares += (k ===1 && !(i ===2 || i ===3 || i ===8 || i ===9)) ? `wall-left ` : ``
-        squares += (k===10 && !(i ===2 || i ===3 || i ===8 || i ===9)) ? `wall-right ` : ``
+        squares += (i ===0 && !(k ===2 || k ===3 || k ===8 || k ===9)) ? `wall-top ` : ``
+        squares += (i ===11 && !(k ===2 || k ===3 ||  k ===8 || k ===9)) ? `wall-bottom ` : ``
+        squares += (k ===0 && !(i ===2 || i ===3 || i ===8 || i ===9)) ? `wall-left ` : ``
+        squares += (k===11 && !(i ===2 || i ===3 || i ===8 || i ===9)) ? `wall-right ` : ``
 
         // add tunnels x8
-        squares += (i ===1 && (k ===2 || k ===3) ) ? `tunnel-top ` : ``
-        squares += (i ===1 && (k ===8 || k ===9) ) ? `tunnel-top ` : ``
-        squares += (i ===10 && (k ===2 || k ===3) ) ? `tunnel-bottom ` : ``
-        squares += (i ===10 && (k ===8 || k ===9) ) ? `tunnel-bottom ` : ``
+        squares += (i ===0 && (k ===2 || k ===3) ) ? `tunnel-top ` : ``
+        squares += (i ===0 && (k ===8 || k ===9) ) ? `tunnel-top ` : ``
+        squares += (i ===11 && (k ===2 || k ===3) ) ? `tunnel-bottom ` : ``
+        squares += (i ===11 && (k ===8 || k ===9) ) ? `tunnel-bottom ` : ``
 
-        squares += (k ===1 && (i  ===2 || i ===3) ) ? `tunnel-left ` : ``
-        squares += (k ===1 && (i  ===8 || i ===9) ) ? `tunnel-left ` : ``
-        squares += (k ===10 && (i  ===2 || i ===3) ) ? `tunnel-right ` : ``
-        squares += (k ===10 && (i  ===8 || i ===9) ) ? `tunnel-right ` : ``
+        squares += (k ===0 && (i ===2 || i ===3) ) ? `tunnel-left ` : ``
+        squares += (k ===0 && (i ===8 || i ===9) ) ? `tunnel-left ` : ``
+        squares += (k ===11 && (i ===2 || i ===3) ) ? `tunnel-right ` : ``
+        squares += (k ===11 && (i ===8 || i ===9) ) ? `tunnel-right ` : ``
 
         // pair tunnel sets {1,2,3,4}
-        squares += ((i ===1 || i ===10) && (k ===2 || k ===3)) ? `tunnel-set1 ` : ``
-        squares += ((i ===1 || i ===10) && (k ===8 || k ===9)) ? `tunnel-set2 ` : ``
+        squares += ((i ===0 || i ===11) && (k ===2 || k ===3)) ? `tunnel-set1 ` : ``
+        squares += ((i ===0 || i ===11) && (k ===8 || k ===9)) ? `tunnel-set2 ` : ``
 
-        squares += ((i ===2 || i ===3) && (k ===1 || k ===10)) ? `tunnel-set3 ` : ``
-        squares += ((i ===8 || i ===9) && (k ===1 || k ===10)) ? `tunnel-set4 ` : ``
+        squares += ((i ===2 || i ===3) && (k ===0 || k ===11)) ? `tunnel-set3 ` : ``
+        squares += ((i ===8 || i ===9) && (k ===0 || k ===11)) ? `tunnel-set4 ` : ``
         
         squares += (i === 4 && k === 9) ? `snake ` : ``
         squares += (i === 5 && k === 6) ? `snake ` : ``
