91b20d5f43b15bba2d92b07307371e9333860406
SeemaMistry <seema.m.1996@gmail.com>
2021-10-04 23:29:09 -0400

    Refactor classList of cells. Remove .cell.tunnel {background = none} and in for loop in app.js, add additional coniditions to prevent wall-SIDE from making a border on border location.

--- snakeGame/app.js
+++ snakeGame/app.js
@@ -11,19 +11,18 @@ for (let i=0; i < 10; i++) {
     for (let k=0; k < 10; k++) {
         squares += `<div class="cell row-${i} col-${k} `
 
-        // checkered board
+        // checkered board - light/dark grey
         squares += (rowCount % 2 === 0 && cellCount % 2 === 0) ? `dark ` : `` // even row, dark on even cell
         squares += (rowCount % 2 !== 0 && cellCount % 2 !== 0) ? `dark ` : ``// odd rows, dark on odd cells
         
         rowCount += (cellCount % 10 === 0) ? 1 : 0 // next row
         cellCount++
 
-
         // add walls 
-        squares += (i === 0) ? `wall-top ` : ``
-        squares += (i === 9) ? `wall-bottom ` : ``
-        squares += (k === 0) ? `wall-left ` : ``
-        squares += (k === 9) ? `wall-right ` : ``
+        squares += (i === 0 && !(k === 1 || k === 2 || k === 7 || k === 8)) ? `wall-top ` : ``
+        squares += (i === 9 && !(k === 1 || k === 2 || k === 7 || k === 8)) ? `wall-bottom ` : ``
+        squares += (k === 0 && !(i === 1 || i === 2 || i === 7 || i === 8)) ? `wall-left ` : ``
+        squares += (k=== 9 && !(i === 1 || i === 2 || i === 7 || i === 8)) ? `wall-right ` : ``
 
         // add tunnels x8
         squares += (i === 0 && (k === 1 || k === 2) ) ? `tunnel-top ` : ``
@@ -43,6 +42,10 @@ for (let i=0; i < 10; i++) {
         squares += ((i === 1 || i === 2) && (k === 0 || k === 9)) ? `tunnel-set3` : ``
         squares += ((i === 7 || i === 8) && (k === 0 || k === 9)) ? `tunnel-set4` : ``
         
+        squares += (i === 5 && k === 5) ? `snake ` : ``
+        squares += (i === 5 && k === 6) ? `snake ` : ``
+        squares += (i === 5 && k === 4) ? `snake ` : ``
+        squares += (i === 5 && k === 7) ? `snake ` : ``
 
         squares += `"></div>` 
 
