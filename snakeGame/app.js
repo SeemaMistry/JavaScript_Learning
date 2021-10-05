ad109c6f72f20ac51eb551c2ee6801bd0a9ed221
SeemaMistry <seema.m.1996@gmail.com>
2021-10-05 00:29:31 -0400

    Enlarge grid to 12x12

--- snakeGame/app.js
+++ snakeGame/app.js
@@ -7,42 +7,43 @@ let squares = ""
 let cellCount = 1
 let rowCount = 0
 // row = i, columns = k
-for (let i=0; i < 10; i++) {
-    for (let k=0; k < 10; k++) {
+for (let i=0; i < 12; i++) {
+    for (let k=0; k < 12; k++) {
+
         squares += `<div class="cell row-${i} col-${k} `
 
         // checkered board - light/dark grey
         squares += (rowCount % 2 === 0 && cellCount % 2 === 0) ? `dark ` : `` // even row, dark on even cell
         squares += (rowCount % 2 !== 0 && cellCount % 2 !== 0) ? `dark ` : ``// odd rows, dark on odd cells
         
-        rowCount += (cellCount % 10 === 0) ? 1 : 0 // next row
+        rowCount += (cellCount % 12 === 0) ? 1 : 0 // next row
         cellCount++
 
         // add walls 
-        squares += (i === 0 && !(k === 1 || k === 2 || k === 7 || k === 8)) ? `wall-top ` : ``
-        squares += (i === 9 && !(k === 1 || k === 2 || k === 7 || k === 8)) ? `wall-bottom ` : ``
-        squares += (k === 0 && !(i === 1 || i === 2 || i === 7 || i === 8)) ? `wall-left ` : ``
-        squares += (k=== 9 && !(i === 1 || i === 2 || i === 7 || i === 8)) ? `wall-right ` : ``
+        squares += (i ===1 && !(k ===2 || k ===3 || k ===8 || k ===9)) ? `wall-top ` : ``
+        squares += (i ===10 && !(k ===2 || k ===3 ||  k ===8 || k ===9)) ? `wall-bottom ` : ``
+        squares += (k ===1 && !(i ===2 || i ===3 || i ===8 || i ===9)) ? `wall-left ` : ``
+        squares += (k===10 && !(i ===2 || i ===3 || i ===8 || i ===9)) ? `wall-right ` : ``
 
         // add tunnels x8
-        squares += (i === 0 && (k === 1 || k === 2) ) ? `tunnel-top ` : ``
-        squares += (i === 0 && (k === 7 || k === 8) ) ? `tunnel-top ` : ``
-        squares += (i === 9 && (k === 1 || k === 2) ) ? `tunnel-bottom ` : ``
-        squares += (i === 9 && (k === 7 || k === 8) ) ? `tunnel-bottom ` : ``
+        squares += (i ===1 && (k ===2 || k ===3) ) ? `tunnel-top ` : ``
+        squares += (i ===1 && (k ===8 || k ===9) ) ? `tunnel-top ` : ``
+        squares += (i ===10 && (k ===2 || k ===3) ) ? `tunnel-bottom ` : ``
+        squares += (i ===10 && (k ===8 || k ===9) ) ? `tunnel-bottom ` : ``
 
-        squares += (k === 0 && (i  === 1 || i === 2) ) ? `tunnel-left ` : ``
-        squares += (k === 0 && (i  === 7 || i === 8) ) ? `tunnel-left ` : ``
-        squares += (k === 9 && (i  === 1 || i === 2) ) ? `tunnel-right ` : ``
-        squares += (k === 9 && (i  === 7 || i === 8) ) ? `tunnel-right ` : ``
+        squares += (k ===1 && (i  ===2 || i ===3) ) ? `tunnel-left ` : ``
+        squares += (k ===1 && (i  ===8 || i ===9) ) ? `tunnel-left ` : ``
+        squares += (k ===10 && (i  ===2 || i ===3) ) ? `tunnel-right ` : ``
+        squares += (k ===10 && (i  ===8 || i ===9) ) ? `tunnel-right ` : ``
 
         // pair tunnel sets {1,2,3,4}
-        squares += ((i === 0 || i === 9) && (k === 1 || k === 2)) ? `tunnel-set1 ` : ``
-        squares += ((i === 0 || i === 9) && (k === 7 || k === 8)) ? `tunnel-set2 ` : ``
+        squares += ((i ===1 || i ===10) && (k ===2 || k ===3)) ? `tunnel-set1 ` : ``
+        squares += ((i ===1 || i ===10) && (k ===8 || k ===9)) ? `tunnel-set2 ` : ``
 
-        squares += ((i === 1 || i === 2) && (k === 0 || k === 9)) ? `tunnel-set3 ` : ``
-        squares += ((i === 7 || i === 8) && (k === 0 || k === 9)) ? `tunnel-set4 ` : ``
+        squares += ((i ===2 || i ===3) && (k ===1 || k ===10)) ? `tunnel-set3 ` : ``
+        squares += ((i ===8 || i ===9) && (k ===1 || k ===10)) ? `tunnel-set4 ` : ``
         
-        squares += (i === 5 && k === 5) ? `snake ` : ``
+        squares += (i === 4 && k === 9) ? `snake ` : ``
         squares += (i === 5 && k === 6) ? `snake ` : ``
         squares += (i === 5 && k === 4) ? `snake ` : ``
         squares += (i === 5 && k === 7) ? `snake ` : ``
